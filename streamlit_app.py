a2c4323ffc60591acc8a8743052316006f2ea40f
korosemmanuel <114986512+korosemmanuel@users.noreply.github.com>
2024-04-27 16:37:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
 def main():
     # Embedding CSS styles
     st.markdown("""
